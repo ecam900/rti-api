01eeb7044d6c445008cfedb22552c5dc0674d243
Eric Crescioni <ecam900@gmail.com>
2020-03-27 14:30:56 -0400

    Add colors package.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({
@@ -12,5 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow
+      .bold
+  )
 );
