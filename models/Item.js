9100d56c1580913fd814ac593e082f751ad7b3bd
Eric Crescioni <ecam900@gmail.com>
2020-04-03 17:37:52 -0400

    Add Item model.

---
+++ models/Item.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ['Inventory Part', 'Assembly Item'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Please add an item name.'],
+    unique: true,
+    trim: true,
+    maxlength: [100, 'Item name cannot be more than 100 characters'],
+  },
+  slug: String,
+  rl_item: String,
+  salesDescription: {
+    type: String,
+    required: [true, 'Please add an item description.'],
+    maxlength: [500, 'Sales description cannot be more than 500 characters'],
+  },
+  salesPrice: Number,
+  itemCost: Number,
+  purchaseDescription: {
+    type: String,
+    maxlength: [500, 'Sales description cannot be more than 500 characters'],
+  },
+  quantityOnHand: Number,
+  quantityOnSO: Number,
+  quantityOnPO: Number,
+  cubicFeet: Number,
+  upc: Number,
+  spanishDescription: String,
+  length: Number,
+  width: Number,
+  height: Number,
+  htsCode: String,
+  boxesPerItem: Number,
+  hotelAndRest: Boolean,
+  isActive: Boolean,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
